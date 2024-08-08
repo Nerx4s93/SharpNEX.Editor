108085b698ab137c878eb98014be5889f4bfb2f4
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-08 06:58:28 +0300

    Удаление тестовых элеметов управления

--- GUI/FormMain.Designer.cs
+++ GUI/FormMain.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.titleBar1 = new SharpNEX.Editor.UI.TitleBar();
-            this.dockPanel1 = new DockPanelControler.DockPanel();
-            this.dockPanel2 = new DockPanelControler.DockPanel();
             this.SuspendLayout();
             // 
             // titleBar1
@@ -45,45 +43,15 @@
             this.titleBar1.LogoColorFormNotActive = System.Drawing.Color.Gray;
             this.titleBar1.Margin = new System.Windows.Forms.Padding(2);
             this.titleBar1.Name = "titleBar1";
-            this.titleBar1.Size = new System.Drawing.Size(986, 45);
+            this.titleBar1.Size = new System.Drawing.Size(990, 45);
             this.titleBar1.TabIndex = 0;
             this.titleBar1.Text = "FormMain";
-            // 
-            // dockPanel1
-            // 
-            this.dockPanel1.BackColorOnFormMove = System.Drawing.Color.Silver;
-            this.dockPanel1.BodyPanelBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.dockPanel1.Location = new System.Drawing.Point(35, 71);
-            this.dockPanel1.Name = "dockPanel1";
-            this.dockPanel1.OutlineColorOnFormEnter = System.Drawing.Color.Red;
-            this.dockPanel1.OutlineColorOnFormMove = System.Drawing.Color.Gray;
-            this.dockPanel1.OutlineWidth = 3F;
-            this.dockPanel1.Size = new System.Drawing.Size(326, 237);
-            this.dockPanel1.TabIndex = 1;
-            this.dockPanel1.Text = "dockPanel1";
-            this.dockPanel1.TitleBarPanelBackColor = System.Drawing.Color.Silver;
-            // 
-            // dockPanel2
-            // 
-            this.dockPanel2.BackColorOnFormMove = System.Drawing.Color.Silver;
-            this.dockPanel2.BodyPanelBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.dockPanel2.Location = new System.Drawing.Point(438, 118);
-            this.dockPanel2.Name = "dockPanel2";
-            this.dockPanel2.OutlineColorOnFormEnter = System.Drawing.Color.Red;
-            this.dockPanel2.OutlineColorOnFormMove = System.Drawing.Color.Gray;
-            this.dockPanel2.OutlineWidth = 3F;
-            this.dockPanel2.Size = new System.Drawing.Size(373, 253);
-            this.dockPanel2.TabIndex = 2;
-            this.dockPanel2.Text = "dockPanel2";
-            this.dockPanel2.TitleBarPanelBackColor = System.Drawing.Color.Silver;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(986, 585);
-            this.Controls.Add(this.dockPanel2);
-            this.Controls.Add(this.dockPanel1);
+            this.ClientSize = new System.Drawing.Size(990, 635);
             this.Controls.Add(this.titleBar1);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MinimumSize = new System.Drawing.Size(274, 279);
@@ -96,7 +64,5 @@
         #endregion
 
         private UI.TitleBar titleBar1;
-        private DockPanelControler.DockPanel dockPanel1;
-        private DockPanelControler.DockPanel dockPanel2;
     }
 }
